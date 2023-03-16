a76dd6945d54adb4c203037f1d65de7920a262e1
sebygalla <sebygalla@hotmail.com>
2023-03-16 11:45:19 -0300

    carrousel

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -9,43 +9,43 @@ import alabanzaCuatro from './../asset/img/4.jpg'
 function UncontrolledExample() {
   return (
     <Carousel>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={alabanzaUno}
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3 className='h1'>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={alabanzaCuatro}
-          alt="Second slide"
-        />
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+             src={alabanzaUno}
+             alt="First slide"
+            />
+            <Carousel.Caption>
+            <h3 className='h1'>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src={alabanzaCuatro}
+            alt="Second slide"
+            />
 
-        <Carousel.Caption>
-          <h3 className='h1'>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={alabanzaTres}
-          alt="Third slide"
-        />
+            <Carousel.Caption>
+            <h3 className='h1'>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src={alabanzaTres}
+            alt="Third slide"
+            />
 
-        <Carousel.Caption>
-          <h3 className='h1'>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
+            <Carousel.Caption>
+            <h3 className='h1'>Third slide label</h3>
+            <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+            </Carousel.Caption>
+        </Carousel.Item>
     </Carousel>
   );
 }
