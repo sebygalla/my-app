88d89e2f331732fd8bc092c8c5d550c9f62d7e20
sebygalla <sebygalla@hotmail.com>
2023-03-20 10:28:32 -0300

    borre todo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react'; 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Barra from './components/NavBar.js'
-import UncontrolledExample from './components/carousel';
-import GridExample from './components/cards';
+
 
 
 function App() {
     return (
       <div className="App">
       
-        {/*barra de navegacion */}
-        <Barra/>
-        {/*carrousel de imagenes */}
-        <UncontrolledExample/>
-
-        <GridExample/>
- 
-
-    
 
       </div>
     );
